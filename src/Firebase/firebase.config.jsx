8066bcee910be30618734372d75c2061c3bedec8
Toever1503 <60539822+Toever1503@users.noreply.github.com>
2022-02-18 17:52:14 +0700

    Update firebase.config.jsx

--- src/Firebase/firebase.config.jsx
+++ src/Firebase/firebase.config.jsx
@@ -9,13 +9,6 @@ import { getStorage, ref } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAT1Y57owZGMLzwO84zuG8gZs9iniQdi9M",
-  authDomain: "reactjs-a37cf.firebaseapp.com",
-  projectId: "reactjs-a37cf",
-  storageBucket: "reactjs-a37cf.appspot.com",
-  messagingSenderId: "369110282575",
-  appId: "1:369110282575:web:0fbb2ebbab860b4f80553a",
-  measurementId: "G-TH8GZYMV8P",
 };
 
 // Initialize Firebase
